f23b980430877b831953b89eb2bf2a735fb210bd
jaanonim <mat8mro@gmail.com>
2021-08-24 15:34:34 +0200

    rewrite server (support for socketio)

--- vue/server/app.py
+++ vue/server/app.py
@@ -1,63 +1,122 @@
-import requests
-from data import Data
-from flask import Flask, render_template, jsonify, request, Response
-from flask_cors import CORS
+from flask import Flask, abort, request
+from flask_login import LoginManager, UserMixin, current_user, login_user
+from flask_socketio import SocketIO, emit
 from gpiozero import CPUTemperature
-from flask_jwt import JWT, jwt_required, current_identity
-from werkzeug.security import safe_str_cmp
 
-username_table = {u.username: u for u in Data.users}
-userid_table = {u.id: u for u in Data.users}
-
-def authenticate(username, password):
-    user = username_table.get(username, None)
-    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
-        return user
-
-def identity(payload):
-    user_id = payload['identity']
-    return userid_table.get(user_id, None)
+from data import Data
+from server import Server
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = Data.secretKey
-app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=7)
-
-CORS(app)
-jwt = JWT(app, authenticate, identity)
-
-isma = "http://192.168.0.11/isma/"
-ismp = "http://192.168.0.15/ismp/"
+app.config["SECRET_KEY"] = Data.secretKey
+app.config["SERVER"] = Server.getInstance()
+login = LoginManager(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
+@login.user_loader
+def user_loader(id):
+    return User(id)
 
-@app.route('/api/')
-@app.route('/api')
-@jwt_required()
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    print(username, password, str(username not in Data.users), Data.users[username])
+    if username not in Data.users or Data.users[username] != password:
+        abort(401)
+    login_user(User(username))
+    return ""
+
+
+"""
 def api():
-	cpu = CPUTemperature()
-	return jsonify(temp = cpu.temperature,isma=isma,ismp=ismp)
+    cpu = CPUTemperature()
+    temp=cpu.temperature
+"""
 
-@app.route('/ismp/<path:path>/')
-@app.route('/ismp/<path:path>')
-@jwt_required()
-def ISMPpath(path):
-    return requests.get(ismp+path,request.args).text
 
-@app.route('/isma/<path:path>/')
-@app.route('/isma/<path:path>')
-@jwt_required()
-def ISMApath(path):
-    return requests.get(isma+path,request.args).text
-	
-@app.route('/ismp/')
-@app.route('/ismp')
-def ISMP():
-	return redirect(ismp, code=302)
-	
-@app.route('/isma/')
-@app.route('/isma')
-def ISMA():
-	return redirect(isma, code=302)
+@socketio.on("connect")
+def on_connect():
+    if current_user.is_anonymous:
+        return False
 
-if __name__ == '__main__':
-    app.run()
+
+@socketio.on("set")
+def _set(json):
+
+    try:
+        name = json["addres"]
+    except:
+        emit(
+            "set",
+            {
+                "name": None,
+                "payload": {"error": "Invalid json (the address is missing)"},
+            },
+        )
+        return
+
+    try:
+        payload = json["payload"]
+    except:
+        emit(
+            "set",
+            {
+                "name": name,
+                "payload": {"error": "Invalid json (something is wrong with payload)"},
+            },
+        )
+        return
+
+    if not Server.getInstance().isName(name):
+        emit("set", {"name": name, "payload": {"error": "Invalid addres"}})
+        return
+
+    data = Server.getInstance().send_set(name, payload)
+    if data["error"]:
+        emit("set", {"name": name, "payload": {"error": data["info"]}})
+        return
+
+    emit("set", {"name": name, "payload": None})
+
+
+@socketio.on("get")
+def _get(json):
+
+    try:
+        name = json["addres"]
+    except:
+        emit(
+            "get",
+            {
+                "name": None,
+                "payload": {"error": "Invalid json (the address is missing)"},
+            },
+        )
+        return
+
+    if name == "GAPI":
+        cpu = CPUTemperature()
+        emit("get", {"name": name, "payload": {"data": {"temp": cpu.temperature}}})
+        return
+
+    if not Server.getInstance().isName(name):
+        emit("get", {"name": name, "payload": {"error": "Invalid addres"}})
+        return
+
+    data = Server.getInstance().send_get(name)
+    if data["error"]:
+        emit("get", {"name": name, "payload": {"error": data["info"]}})
+        return
+
+    emit("get", {"name": name, "payload": {"data": data["info"]}})
+
+
+if __name__ == "__main__":
+    socketio.run(app, host=Data.addres, port="5000")
---
+++ vue/server/server.py
@@ -0,0 +1,162 @@
+import json
+import socket
+import sys
+import threading
+import time
+
+from data import Data
+
+
+class Server:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if Server.__instance == None:
+            Server()
+        return Server.__instance
+
+    def __init__(self):
+
+        if Server.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Server.__instance = self
+
+        self.clients = {}
+        self.size = 2048
+        self.port = 2693
+        self.addres = Data.addres
+        self.command = {}
+        self.data = {}
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.server.bind((self.addres, self.port))
+        except Exception as e:
+            print(f"[SERVER] {e}")
+            exit()
+        print("[SERVER] Starting...")
+        thread = threading.Thread(name="server", target=self.run, daemon=True)
+        thread.start()
+
+    def handle_client(self, conn, addr):
+        print(f"[SERVER] {addr} connected.")
+
+        left = 3
+        while left > 0:
+            msg = conn.recv(self.size).decode()
+            if msg:
+                try:
+                    code, name = msg.split(":", 1)
+                except:
+                    code, name = None, None
+                if code == "CON":
+                    if self.clients.get(name):
+                        if self.ping(name):
+                            conn.send(
+                                str.encode(
+                                    "ERR:Name is taken! Pleace selcet other name."
+                                )
+                            )
+                            left -= 1
+                            continue
+                    print(f"[SERVER] {addr} registered with name: {name}.")
+                    conn.send(str.encode("OK:Connected sucessfuly"))
+                    self.clients[name] = conn
+                    self.command[name] = None
+                    self.data[name] = None
+                    self.listen(name)
+            left -= 1
+        print(f"[SERVER] {addr} timeout.")
+        conn.close()
+        sys.exit()
+
+    def listen(self, name):
+        while True:
+            msg = self.clients[name].recv(self.size).decode()
+            if not msg:
+                continue
+            code, info = msg.split(":", 1)
+            print(f"[SERVER] {code} : {info}")
+            if self.command[name]:
+                if self.command[name] == "GET":
+                    if code != "DATA":
+                        self.data[name] = {"error": True, "info": "Wrong response"}
+                    else:
+                        self.data[name] = {"error": False, "info": json.loads(info)}
+                elif self.command[name] == "SET":
+                    if code == "ERR":
+                        self.data[name] = {"error": True, "info": info}
+                    elif code != "OK":
+                        self.data[name] = {"error": True, "info": "Wrong response"}
+                    else:
+                        self.data[name] = {"error": False, "info": "OK"}
+                self.command[name] = None
+                continue
+            if code == "PING":
+                self.send("OK:OK")
+            elif code == "DATA":
+                data = json.loads(info)
+                print(data)
+                self.send("OK:OK")
+            elif code == "OK":
+                continue
+
+    def ping(self, name):
+        other = self.clients.get(name)
+        try:
+            other.send(str.encode(f"PING:{name}"))
+            msg = other.recv(self.size).decode()
+            if not "OK" in msg:
+                raise
+        except:
+            print(f"[SERVER] {name} diconnected (not responding).")
+            other.close()
+            self.clients.pop(name)
+            self.command.pop(name)
+            self.data.pop(name)
+            return False
+        print(f"[SERVER] {name} is still alive.")
+        return True
+
+    def send_set(self, name, data):
+        self.data[name] = None
+        self.command[name] = "SET"
+        self.send(f"SET:{json.dumps(data)}", name)
+        while self.data[name] is None:
+            pass
+        return self.data[name]
+
+    def send_get(self, name):
+        self.data[name] = None
+        self.command[name] = "GET"
+        self.send(f"GET:GET", name)
+        while self.data[name] is None:
+            pass
+        return self.data[name]
+
+    def isName(self, name):
+        c = self.clients.get(name)
+        if c is None:
+            return False
+        return True
+
+    def send(self, msg, name):
+        c = self.clients.get(name)
+        message = msg.encode()
+        c.send(message)
+
+    def run(self):
+        self.server.listen()
+        print(f"[SERVER] Server is listening on {self.port}")
+        while True:
+            conn, addr = self.server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
+
+
+if __name__ == "__main__":
+    Server()
+    while True:
+        pass
