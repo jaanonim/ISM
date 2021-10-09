d22f35590e9edbc5eb743ea53a3bb43b36be31e9
jaanonim <mat8mro@gmail.com>
2021-10-09 14:28:47 +0200

    fixing

--- send_backend.bat
+++ send_backend.bat
@@ -1,5 +1,6 @@
 ssh pi@192.168.0.26 -p 777 ./send_backend
 scp -P 777 E:\Mateusz\dodatki\PROGRAMOWANIE\ISM\server\app.py pi@192.168.0.26:~/ism/server/app.py
 scp -P 777 E:\Mateusz\dodatki\PROGRAMOWANIE\ISM\server\server.py pi@192.168.0.26:~/ism/server/server.py
+scp -P 777 E:\Mateusz\dodatki\PROGRAMOWANIE\ISM\server\data.py pi@192.168.0.26:~/ism/server/data.py
 scp -P 777 E:\Mateusz\dodatki\PROGRAMOWANIE\ISM\server\user.py pi@192.168.0.26:~/ism/server/user.py
 scp -P 777 E:\Mateusz\dodatki\PROGRAMOWANIE\ISM\server\requirements.txt pi@192.168.0.26:~/ism/server/requirements.txt
--- server/data.py
+++
@@ -1,8 +0,0 @@
-from user import User
-
-
-class Data:
-
-    users = [User(1, "jaanonim", "mateusz1234")]
-    secretKey = "drgfbhjrgsdgfaeutgfaes5"
-    addres = "192.168.0.27"
