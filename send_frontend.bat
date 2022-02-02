1f8043afad7c50b40eb2a410cca9bf925c0ae75d
Jaanonim <mat8mro@gmail.com>
2022-02-02 19:00:35 +0100

    fix path

--- send_frontend.bat
+++ send_frontend.bat
@@ -1,5 +1,5 @@
 ssh pi@192.168.0.26 -p 777 ./send_frontend
-cd E:\Mateusz\dodatki\PROGRAMOWANIE\ISM\ism_vue
+cd C:\Data\Programowanie\ISM
 call npm run build
-scp -r -P 777 E:\Mateusz\dodatki\PROGRAMOWANIE\ISM\ism_vue\dist pi@192.168.0.26:~/ism/
+scp -r -P 777 C:\Data\Programowanie\ISM\ism_vue\dist pi@192.168.0.26:~/ism/
 cd ..
