e87e8a71d160958cd48f93a6c4225bdcccf58ec5
Jaanonim <mat8mro@gmail.com>
2022-02-12 15:56:06 +0100

    new ip

--- send_frontend.bat
+++ send_frontend.bat
@@ -1,5 +1,5 @@
-ssh pi@192.168.0.26 -p 777 ./send_frontend
-cd C:\Data\Programowanie\ISM
+ssh pi@192.168.1.150 -p 777 ./send_frontend
+cd C:\Data\Programowanie\ISM\ism_vue
 call npm run build
-scp -r -P 777 C:\Data\Programowanie\ISM\ism_vue\dist pi@192.168.0.26:~/ism/
+scp -r -P 777 C:\Data\Programowanie\ISM\ism_vue\dist pi@192.168.1.150:~/ism/
 cd ..
